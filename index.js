052e7c3156a49f6ce7202ab776ab6facda599b58
samuelfsd <samuel.estudo778@gmail.com>
2024-08-09 10:23:44 -0300

    feat: add delay

--- index.js
+++ index.js
@@ -1,12 +1,28 @@
 const puppeteer = require('puppeteer-core');
 
+const url = 'https://www8.receita.fazenda.gov.br/SimplesNacional/Aplicacoes/ATSPO/pgmei.app/Identificacao';
+let googleURL = '/usr/bin/google-chrome';
+
+
 (async () => {
-  const browser = await puppeteer.launch({executablePath: '/usr/bin/google-chrome'});
+  const browser = await puppeteer.launch({executablePath: googleURL });
   const page = await browser.newPage();
 
-  // Navigate the page to a URL.
-  await page.goto('https://www8.receita.fazenda.gov.br/SimplesNacional/Aplicacoes/ATSPO/pgmei.app/Identificacao');
+  await page.goto(url);
+
+  await page.type('#cnpj', '00000000000191')
+
+  await page.click('[type=submit]')
+
+  await delay(4000)
   await page.screenshot({path: 'farm-site.png'})
+
 
   await browser.close()
 })();
+
+function delay(time) {
+  return new Promise(function(resolve) {
+    setTimeout(resolve, time)
+  });
+}
